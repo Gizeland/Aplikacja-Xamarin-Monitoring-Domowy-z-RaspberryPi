36b9d4670d4006a8e8a379cbce604ffdbd52fb2d
maciejpawlo <maciejpawlo@gmail.com>
2020-01-12 19:58:18 +0100

    Initial check-in of module loginCamera

---
+++ loginCamera/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace loginCamera
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public static string playerUri;
+        public MainPage()
+        {
+            InitializeComponent(); 
+        }
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            string login = string.Concat(Login.Text,":");
+            string pass = string.Concat(Password.Text,"@");
+            string url = tbUrl.Text;
+
+            string uri = string.Concat("http://",login,pass,url);
+            playerUri = uri;
+
+            
+            Application.Current.MainPage = new NavigationPage(new PlayerPage());
+
+        }
+        
+    }
+
+    
+}
---
+++ loginCamera/PlayerPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using LibVLCSharp.Shared;
+using Xamarin.Forms;
+
+namespace loginCamera
+{
+    public partial class PlayerPage : ContentPage
+    {
+        LibVLC _libvlc;
+        public PlayerPage()
+        {
+            InitializeComponent();
+            Core.Initialize();
+            _libvlc = new LibVLC();
+            PlayerInit();
+        }
+
+        private void PlayerInit()
+        {
+            var media = new Media(_libvlc, MainPage.playerUri, FromType.FromLocation);
+            myVideo.MediaPlayer = new MediaPlayer(media) { EnableHardwareDecoding = true };
+            myVideo.MediaPlayer.Play();
+        }
+
+        private void btBackClick(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+            myVideo.MediaPlayer.Stop();
+        }
+    }
+}
